8fdde4443780fe78c82210d29b0e2585d30bda79
Federico Angert <federico.angert@gmail.com>
2023-09-01 11:46:39 +0200

    prentrega3termuinada

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
-// Cotizador de Seguros
+// Limpiar datos almacenados en Local Storage al cargar la página
+localStorage.removeItem('datosSeguro');
 
-// Objetos
-const segurosDisponibles = [
-    { tipo: '1', descripcion: 'Seguro de Auto' },
-    { tipo: '2', descripcion: 'Seguro de Bicicleta' }
-];
+// Objeto con seguros disponibles en formato JSON
+const segurosJSON = '[{"tipo": "1", "descripcion": "Seguro de Auto"}, {"tipo": "2", "descripcion": "Seguro de Bicicleta"}]';
+const segurosDisponibles = JSON.parse(segurosJSON);
 
-// Código para calcular precio
+// Obtener elementos del DOM
+const tipoSeguroElement = document.getElementById('tipoSeguro');
+const valorSeguroElement = document.getElementById('valorSeguro');
+const calcularButton = document.getElementById('calcular');
+const resultadoElement = document.getElementById('resultado');
+
+// Llenar opciones de tipo de seguro desde el objeto
+segurosDisponibles.forEach(seguro => {
+    const option = document.createElement('option');
+    option.value = seguro.tipo;
+    option.textContent = seguro.descripcion;
+    tipoSeguroElement.appendChild(option);
+});
+
+// Función para calcular el precio del seguro
+function calcularPrecio() {
+    const tipoSeguro = tipoSeguroElement.value;
+    const valorSeguro = parseFloat(valorSeguroElement.value);
+
+    if (isNaN(valorSeguro) || valorSeguro <= 0) {
+        resultadoElement.textContent = 'Ingrese un valor de seguro válido.';
+        return;
+    }
+
+    const precio = queSeguro(tipoSeguro, valorSeguro);
+
+    if (precio >= 0) {
+        resultadoElement.textContent = `El precio de su seguro es $${precio.toFixed(2)} al mes.`;
+
+        // Almacenar valores en el Local Storage
+        const datosSeguro = { tipoSeguro, valorSeguro };
+        localStorage.setItem('datosSeguro', JSON.stringify(datosSeguro));
+    } else {
+        resultadoElement.textContent = 'Debe seleccionar "Seguro de Auto" (1) o "Seguro de Bicicleta" (2).';
+    }
+}
+
+// Manejador de evento para el botón "Calcular"
+calcularButton.addEventListener('click', calcularPrecio);
+
+// Función para calcular el precio del seguro
 function queSeguro(tipoSeguro, valorSeguro) {
     let precio = 0;
 
@@ -15,52 +54,19 @@ function queSeguro(tipoSeguro, valorSeguro) {
     } else if (tipoSeguro === '2') {
         precio = valorSeguro * 0.019;
     } else {
-        alert('El número de seguro ingresado no existe, intente nuevamente');
-        // alerta en caso de error
-        return -1; // retorna un número inválido para indicar el error
+        return -1;
     }
 
     return precio;
 }
 
-//Código selección de seguro y valor
-function cotizarSeguro() {
-    alert('Bienvenido al cotizador de seguros');
-    alert('Seleccione el tipo de seguro:');
-
-    for (const seguro of segurosDisponibles) {
-        alert(`${seguro.tipo}. ${seguro.descripcion}`);
-    }
-
-    let tipoSeguro = '';
-    while (tipoSeguro !== '1' && tipoSeguro !== '2') {
-        tipoSeguro = prompt('Ingrese el tipo de seguro:');
-        if (tipoSeguro !== '1' && tipoSeguro !== '2') {
-            alert('El tipo de seguro ingresado no es válido, inténtelo nuevamente');
-        }
-    }
-
-    let valorSeguro = 0;
-    while (true) {
-        valorSeguro = prompt('Ingrese el valor de su artículo a asegurar:');
-        if (valorSeguro !== '') {
-            break;
-        }
-        alert('El valor del seguro ingresado no es válido, inténtelo nuevamente');
-    }
-
-    return [tipoSeguro, Number(valorSeguro)];
-}
-
-//Llamado funciones
-const [seguroSelec, precioSegSec] = cotizarSeguro();
-
-const precio = queSeguro(seguroSelec, precioSegSec);
-
-if (precio >= 0) {
-    alert(`El precio de su seguro es $${precio.toFixed(2)} al mes.`);
-} else {
-    alert('Debe ingresar "1" si desea seleccionar el seguro de Auto o "2" si desea el seguro de Bicicleta, refresque la página e intente nuevamente');
+// Cargar datos almacenados en Local Storage si existen
+const datosSeguroJSON = localStorage.getItem('datosSeguro');
+if (datosSeguroJSON) {
+    const datosSeguro = JSON.parse(datosSeguroJSON);
+    tipoSeguroElement.value = datosSeguro.tipoSeguro;
+    valorSeguroElement.value = datosSeguro.valorSeguro;
+    calcularPrecio(); // Calcular el precio con los datos almacenados
 }
 
 
